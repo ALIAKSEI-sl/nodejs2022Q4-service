422a1b0df6753cd9bfe4da9d4fe199802e92cb60
Aliaksei <sladkiy.aleksey@gmail.com>
2023-02-03 13:07:08 +0100

    feat: add global validation pipe

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 import 'dotenv/config';
 
 const PORT = process.env.PORT || 4000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({ enableDebugMessages: true, whitelist: true }),
+  );
+
   await app.listen(PORT);
 }
 bootstrap();
