f37cc6426e29bff383b9036bcfde60e34943a962
Aliaksei <sladkiy.aleksey@gmail.com>
2023-02-03 14:17:02 +0100

    fix: edited response messages

--- src/helpers/responseMessages.ts
+++ src/helpers/responseMessages.ts
@@ -1,16 +1,8 @@
-export const enum StatusCode {
-  ok = 200,
-  created = 201,
-  noContent = 204,
-  badRequest = 400,
-  forbidden = 403,
-  notFound = 404,
-  unprocessableEntity = 422,
-  internalServerError = 500,
-}
-
 export const enum ErrorMessages {
   nonExistentUser = "UserId doesn't exist",
+  nonExistentTrack = "Track doesn't exist",
+  nonExistentAlbum = "Album doesn't exist",
+  nonExistentArtist = "Artist doesn't exist",
   nonExistentEndpoint = 'Endpoint does not exist',
   notUuid = "UserId isn't uuid",
   serverError = 'Sorry, error on the server',
@@ -18,4 +10,6 @@ export const enum ErrorMessages {
   invalidJson = 'Invalid JSON request',
   requiredFields = "Required fields are missing or don't match the type",
   createdUser = 'A new record has been created',
+  equalPasswords = 'The new password cannot equal the old password',
+  incorrectPassword = 'Incorrect password entered',
 }
