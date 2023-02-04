c888194879dc2df9ca5cdda27ed0095eefbd1650
Aliaksei <sladkiy.aleksey@gmail.com>
2023-02-04 21:55:44 +0100

    feat: read specifications

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule } from '@nestjs/swagger';
+import { readFile } from 'fs/promises';
+import { join } from 'node:path';
+import { parse } from 'yaml';
 import 'dotenv/config';
 
 const PORT = process.env.PORT || 4000;
@@ -12,6 +16,11 @@ async function bootstrap() {
     new ValidationPipe({ enableDebugMessages: true, whitelist: true }),
   );
 
+  const pathDoc = join(__dirname, '..', 'doc/api.yaml');
+  const contentDoc = await readFile(pathDoc, 'utf-8');
+  SwaggerModule.setup('doc', app, parse(contentDoc));
+
   await app.listen(PORT);
 }
+
 bootstrap();
