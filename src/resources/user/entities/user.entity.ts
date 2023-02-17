167be6822bbf744865e877b28535faae680afe4e
Aliaksei <sladkiy.aleksey@gmail.com>
2023-02-17 17:46:37 +0100

    feat: changed DB in user

--- src/resources/user/entities/user.entity.ts
+++ src/resources/user/entities/user.entity.ts
@@ -1,22 +1,38 @@
-import { Exclude } from 'class-transformer';
+import { Transform, Exclude } from 'class-transformer';
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  VersionColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 
+@Entity('user')
 export class UserEntity {
-  constructor(object: UserEntity) {
-    this.id = object.id;
-    this.login = object.login;
-    this.password = object.password;
-    this.version = object.version;
-    this.createdAt = object.createdAt;
-    this.updatedAt = object.updatedAt;
+  constructor(login: string, password: string) {
+    this.login = login;
+    this.password = password;
   }
 
+  @PrimaryGeneratedColumn('uuid')
   id: string; // uuid v4
+
+  @Column()
   login: string;
 
+  @Column()
   @Exclude()
   password: string;
 
+  @VersionColumn()
   version: number; // integer number, increments on update
+
+  @CreateDateColumn()
+  @Transform(({ value }) => new Date(value).getTime())
   createdAt: number; // timestamp of creation
+
+  @UpdateDateColumn()
+  @Transform(({ value }) => new Date(value).getTime())
   updatedAt: number; // timestamp of last update
 }
--- src/resources/user/user.service.ts
+++ src/resources/user/user.service.ts
@@ -2,51 +2,53 @@ import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdatePasswordDto } from './dto/update-password.dto';
 import { UserEntity } from './entities/user.entity';
-import { InMemoryDb } from '../../db/db.service.db';
 import { HttpStatus } from '@nestjs/common';
 import { createHttpException } from '../../helpers/createHttpException';
 import { ErrorMessages } from '../../helpers/responseMessages';
-import { v4 as uuidv4 } from 'uuid';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class UserService {
-  constructor(private db: InMemoryDb) {}
+  constructor(
+    @InjectRepository(UserEntity)
+    private userRepository: Repository<UserEntity>,
+  ) {}
 
-  create(createUserDto: CreateUserDto): UserEntity {
-    const date = Date.now();
-    const user = new UserEntity({
-      id: uuidv4(),
-      login: createUserDto.login,
-      password: createUserDto.password,
-      version: 1,
-      createdAt: date,
-      updatedAt: date,
+  async create(createUserDto: CreateUserDto): Promise<UserEntity> {
+    const existenceUser = await this.userRepository.findOne({
+      where: { login: createUserDto.login },
     });
+    if (existenceUser) {
+      createHttpException(
+        ErrorMessages.userAlreadyExists,
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+    const user = new UserEntity(createUserDto.login, createUserDto.password);
 
-    this.db.users.push(user);
-    return user;
+    const createdUser = this.userRepository.create(user);
+    const savedUser = await this.userRepository.save(createdUser);
+    return savedUser;
   }
 
-  findAll(): UserEntity[] {
-    const users = this.db.users;
+  async findAll(): Promise<UserEntity[]> {
+    const users = await this.userRepository.find();
     return users;
   }
 
-  findOne(id: string): UserEntity | undefined {
-    const user = this.db.users.find(({ id: userId }) => userId === id);
+  async findOne(id: string): Promise<UserEntity> | null {
+    const user = await this.userRepository.findOne({ where: { id } });
     return user;
   }
 
-  update(
+  async update(
     id: string,
     updatePasswordDto: UpdatePasswordDto,
-  ): UserEntity | undefined {
-    const userIndex = this.db.users.findIndex(
-      ({ id: userId }) => userId === id,
-    );
-    if (userIndex === -1) return undefined;
+  ): Promise<UserEntity> {
+    const user = await this.userRepository.findOne({ where: { id } });
+    if (user === null) return null;
 
-    const user = this.db.users[userIndex];
     if (user.password === updatePasswordDto.newPassword) {
       createHttpException(ErrorMessages.equalPasswords, HttpStatus.FORBIDDEN);
     } else if (user.password !== updatePasswordDto.oldPassword) {
@@ -56,22 +58,15 @@ export class UserService {
       );
     }
 
-    const date = Date.now();
     user.password = updatePasswordDto.newPassword;
-    user.version++;
-    user.updatedAt = date;
-
-    return user;
+    const savedUser = await this.userRepository.save(user);
+    return savedUser;
   }
 
-  remove(id: string): void {
-    const userIndex = this.db.users.findIndex(
-      ({ id: userId }) => userId === id,
-    );
-    if (userIndex === -1) {
+  async remove(id: string): Promise<void> {
+    const deletedUser = await this.userRepository.delete(id);
+    if (deletedUser.affected === 0) {
       createHttpException(ErrorMessages.nonExistentUser, HttpStatus.NOT_FOUND);
     }
-
-    this.db.users.splice(userIndex, 1);
   }
 }
